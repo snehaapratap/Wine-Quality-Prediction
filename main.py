7ada919a4ce34cff80779514643501c3302cc01e
Sneha P Pratap <snehapratap248@gmail.com>
2025-01-15 01:36:52 +0530

    validation done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.wine.utils import logger
 from src.wine.utils.common import read_yaml, create_directories
 from src.wine.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.wine.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 
@@ -9,6 +10,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.initiate_data_validation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
