03c84354dd543ebcd8b3d10e7aef3be804dba722
Sneha P Pratap <snehapratap248@gmail.com>
2025-01-15 03:05:00 +0530

    transformation done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.wine.utils import logger
 from src.wine.utils.common import read_yaml, create_directories
 from src.wine.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.wine.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.wine.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+
 
 
 
@@ -20,6 +22,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
